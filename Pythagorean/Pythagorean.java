5a59516f4ea692070b0782426ab93f229fc1270c
JFLin <35427835+jlin728@users.noreply.github.com>
2018-03-05 20:18:26 -0500

    Java
    ● Importing
    ● Object Instantiation
    ● Method Invocation
    ● java.lang package

---
+++ Pythagorean/Pythagorean.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+
+public class Pythagorean {
+    public double calcHyp (double x, double y){
+        Double sqrt = Math.sqrt((x*x) + (y*y));     
+        return sqrt;
+    }
+}
---
+++ Pythagorean/PythagoreanTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTest {
+    
+    public static void main(String[] args) {
+        Pythagorean iD = new Pythagorean();
+        Double pyTest = iD.calcHyp(2, 3);
+        System.out.println(pyTest);
+    }
+}
